fd3655b72c0658a016fe7f318e70f03640ddd3ff
Tx-wan <77199620+Tx-wan@users.noreply.github.com>
2021-02-08 19:39:53 +0800

    sampledata
    Commit sample data

---
+++ Assignment1/Assignment1/DirWalker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace Assignment1
+{
+  
+
+    public class DirWalker
+    {
+
+        public void walk(String path)
+        {
+
+            string[] list = Directory.GetDirectories(path);
+
+
+            if (list == null) return;
+
+            foreach (string dirpath in list)
+               // foreach (String f : list)
+            {
+                if (Directory.Exists(dirpath))
+                {
+                    walk(dirpath);
+                    Console.WriteLine("Dir:" + dirpath);
+                }
+            }
+            string[] fileList = Directory.GetFiles(path);
+            foreach (string filepath in fileList)
+            {
+
+                    Console.WriteLine("File:" + filepath);
+            }
+        }
+
+//        public static void Main(String[] args)
+//        {
+//            DirWalker fw = new DirWalker();
+//           fw.walk(@"/");
+//        }
+
+    }
+}
---
+++ Assignment1/Assignment1/SimpleCSVParser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Assignment1
+{
+    public class SimpleCSVParser
+    {
+
+
+ //       public static void Main(String[] args)
+ //       {
+ //           SimpleCSVParser parser = new SimpleCSVParser();
+ //           parser.parse(@"/Users/dpenny/Projects/Assignment1/Assignment1/sampleFile.csv");
+ //       }
+
+
+        public void parse(String fileName)
+        {
+            try { 
+            using (TextFieldParser parser = new TextFieldParser(fileName))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                    while (!parser.EndOfData)
+                {
+                    //Process row
+                    string[] fields = parser.ReadFields();
+                    foreach (string field in fields)
+                    {
+                        Console.WriteLine(field);
+                    }
+                }
+            }
+        
+        }catch(IOException ioe){
+                Console.WriteLine(ioe.StackTrace);
+         }
+
+    }
+
+
+    }
+}
